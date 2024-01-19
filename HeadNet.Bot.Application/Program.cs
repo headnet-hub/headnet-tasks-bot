319dffcc9e3f1f38f74f93109456a79c3644f074
Maksim Udincev <maksim.udincev01@mail.ru>
2024-01-19 18:44:43 +0300

    add bot application

---
+++ HeadNet.Bot.Application/Program.cs
@@ -0,0 +1,22 @@
+﻿using HeadNet.Bot.Services;
+
+namespace HeadNet.Bot.Application
+{
+    public abstract class Program
+    {
+        private static void Main(string telegramBotToken)
+        {
+            var taskService = new TaskService();
+            
+            var bot = new Bot(
+                telegramBotToken,
+                taskService,
+                new GoogleIntegrationService(),
+                new ChatStateService(),
+                new TopicService(taskService),
+                new MoodService());
+            
+            bot.Start();
+        }
+    }
+}
